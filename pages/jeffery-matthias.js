7ae7dc62921b57cd5c2eeee7a9df872f4390bcbc
Colby Blaisdell <ccblaisdell@gmail.com>
2023-04-02 16:56:45 -0500

    Attempt 2

--- pages/jeffery-matthias.js
+++ pages/jeffery-matthias.js
@@ -3,13 +3,14 @@ import { speakers } from "../data/speakers";
 
 export default function () {
   const speaker = speakers.find((bio) => bio.slug === "jeffery-matthias");
+  if (!speaker) return null;
   return (
     <BioPage
-      src={speaker?.imgSrc}
-      name={speaker?.name}
-      subtitle={speaker?.subtitle}
+      src={speaker.imgSrc}
+      name={speaker.name}
+      subtitle={speaker.subtitle}
     >
-      <p>{speaker?.description}</p>
+      <p>{speaker.description}</p>
     </BioPage>
   );
 }
