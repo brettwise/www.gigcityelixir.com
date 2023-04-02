66b0f91f15f1bb8c130d3e312b0186c47773e21f
Colby Blaisdell <ccblaisdell@gmail.com>
2023-04-02 16:51:01 -0500

    Add jeffery matthias's bio

--- data/speakers.js
+++ data/speakers.js
@@ -23,8 +23,9 @@ Husband and dad. Food. Like to ask people how they are actually doing.
     slug: "bryan-hunter",
     name: "Bryan Hunter",
     imgSrc: "bryan-hunter.jpg",
-    description: "Bryan Hunter is an Enterprise Fellow at HCA Healthcare. He has three decades of custom software development and consulting experience in complex business domains. In 2012 he founded Nashville Functional Programmers (@NashFP). Bryan enjoys supporting FP communities around the world and has shared his experience in Lean and functional programming at conferences and universities in Oslo, London, Manila, Bangalore, Brisbane, and throughout the United States.",
-    twitter: "@NashFP"
+    description:
+      "Bryan Hunter is an Enterprise Fellow at HCA Healthcare. He has three decades of custom software development and consulting experience in complex business domains. In 2012 he founded Nashville Functional Programmers (@NashFP). Bryan enjoys supporting FP communities around the world and has shared his experience in Lean and functional programming at conferences and universities in Oslo, London, Manila, Bangalore, Brisbane, and throughout the United States.",
+    twitter: "@NashFP",
   },
   { slug: "chris-keathley", name: "Chris Keathley" },
   {
@@ -45,7 +46,13 @@ properties. When not in front of a computer, he loves running and
 spending time with his family.`,
   },
   { slug: "greg-mefford", name: "Greg Mefford" },
-  { slug: "jeffrey-matthias", name: "Jeffrey Matthias" },
+  {
+    slug: "jeffery-matthias",
+    name: "Jeffrey Matthias",
+    imgSrc: "jeffery-matthias.jpg",
+    description:
+      "Jeffrey is a father, a maker, and a principal software engineer at Community.com. He is also the co-author of Testing Elixir, published by PragProg. ",
+  },
   { slug: "joshua-plicque", name: "Joshua Plicque" },
   {
     slug: "kimberly-erni",
@@ -85,7 +92,13 @@ focused on real-time patient care with project Waterpark. POKE 53281,
     imgSrc: "steve-bussey.jpg",
     description: `Steve Bussey is a software architect turned startup founder. He is the author of "Real-Time Phoenix", which came from his work leading development on real-time Elixir applications that power critical features of business software, and he's currently writing "Elixir for Rubyists". Steve is passionate about sharing Elixir due to the positive impact it has had on how he designs and develops applications.`,
   },
-  { slug: "vanessa-lee", name: "Vanessa Lee", imgSrc: "vanessa-lee.jpg", description: "Vanessa is a senior engineering manager with Papa Inc. She's been developing s/w for over 20 years with a focus on the Erlang Ecosystem since 2011. She leads a team of Elixir and QA Engineers to passionately architect and maintain software. In particular, she loves the treasure hunt of troubleshooting a gnarly bug. In her free time, she is a student of Forensic Genealogy and Machine Learning." },
+  {
+    slug: "vanessa-lee",
+    name: "Vanessa Lee",
+    imgSrc: "vanessa-lee.jpg",
+    description:
+      "Vanessa is a senior engineering manager with Papa Inc. She's been developing s/w for over 20 years with a focus on the Erlang Ecosystem since 2011. She leads a team of Elixir and QA Engineers to passionately architect and maintain software. In particular, she loves the treasure hunt of troubleshooting a gnarly bug. In her free time, she is a student of Forensic Genealogy and Machine Learning.",
+  },
   {
     slug: "zach-daniel",
     name: "Zach Daniel",
---
+++ pages/jeffery-matthias.js
@@ -0,0 +1,15 @@
+import { BioPage } from "../components/BioPage";
+import { speakers } from "../data/speakers";
+
+export default function () {
+  const speaker = speakers.find((bio) => bio.slug === "jeffery-matthias");
+  return (
+    <BioPage
+      src={speaker.imgSrc}
+      name={speaker.name}
+      subtitle={speaker.subtitle}
+    >
+      <p>{speaker.description}</p>
+    </BioPage>
+  );
+}
